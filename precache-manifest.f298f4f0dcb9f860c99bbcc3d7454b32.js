0b9d1d0e777a96ac5929328a2b76a2910de5141a
phucpv <phucpv@vng.com.vn>
2019-01-18 04:23:15 +0700

    mc donalds

---
+++ precache-manifest.f298f4f0dcb9f860c99bbcc3d7454b32.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ed2cabf5d6fb426ac89",
+    "url": "/static/css/main.4a219004.chunk.css"
+  },
+  {
+    "revision": "4ed2cabf5d6fb426ac89",
+    "url": "/static/js/main.4ed2cabf.chunk.js"
+  },
+  {
+    "revision": "a31caf016d62fafcc230",
+    "url": "/static/js/1.a31caf01.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce2a4f411fa119c9a4d79cee3b10b71b",
+    "url": "/static/media/item_bigmac@3x.ce2a4f41.png"
+  },
+  {
+    "revision": "6504ee97d5d55675b43d26a3f805e7ff",
+    "url": "/static/media/item_chicken@3x.6504ee97.png"
+  },
+  {
+    "revision": "121d38631d7bf138a6fc162a1e29c388",
+    "url": "/static/media/item_frenchfries@3x.121d3863.png"
+  },
+  {
+    "revision": "d6b0cfd337b3b391e51834ca1e6c42f9",
+    "url": "/static/media/item_happymeal@3x.d6b0cfd3.png"
+  },
+  {
+    "revision": "0a4304c37e39b4a2a7881c895a886a9a",
+    "url": "/static/media/item_rice@3x.0a4304c3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bd4eced986a29f4383601ab40e107dd7",
+    "url": "/static/media/background_home@3x.bd4eced9.jpg"
+  },
+  {
+    "revision": "bbdb0186714683643490e483a3318d24",
+    "url": "/static/media/firewall@3x.bbdb0186.png"
+  },
+  {
+    "revision": "d33bb255d02d11c2346941f678ba0edb",
+    "url": "/static/media/background_playgame@3x.d33bb255.jpg"
+  },
+  {
+    "revision": "cc558bacaed131e8d545aabc30b5cd21",
+    "url": "/static/media/bg_finish_final@3x.cc558bac.png"
+  },
+  {
+    "revision": "600fc753bfea26a17e3d2295be05ba64",
+    "url": "/static/media/item_bigmac_large@3x.600fc753.png"
+  },
+  {
+    "revision": "b718fb2cfc5729b79376cdfe1f560216",
+    "url": "/static/media/item_chicken_large@3x.b718fb2c.png"
+  },
+  {
+    "revision": "063f4607276849d46e89175f59622600",
+    "url": "/static/media/item_frenchfries_large@3x.063f4607.png"
+  },
+  {
+    "revision": "e62cad7cd36a64092e1ae74a27b7f5ee",
+    "url": "/static/media/item_happymeal_large@3x.e62cad7c.png"
+  },
+  {
+    "revision": "699820619c425f7e16660ed600e4d9d8",
+    "url": "/static/media/item_rice_large@3x.69982061.png"
+  },
+  {
+    "revision": "c2d62170c68942bdac3a24107be82805",
+    "url": "/static/media/banner@3x.c2d62170.png"
+  },
+  {
+    "revision": "d4c077febe3e4fe3c0d779ff35eb2129",
+    "url": "/index.html"
+  }
+];
