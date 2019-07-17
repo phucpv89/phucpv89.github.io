7bee1714a215414a3d2b45fb5c91d5b068de3a8b
phucpv <phucpv@vng.com.vn>
2019-07-17 15:31:14 +0700

    s

---
+++ colossus/web/v1/h5payment/precache-manifest.455b1c8b4a938776c9e20aa6cf76b584.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e166526f34066e696aa81d0b44d473cc",
+    "url": "/colossus/web/v1/h5payment/index.html"
+  },
+  {
+    "revision": "339b9959881f07854a94",
+    "url": "/colossus/web/v1/h5payment/static/css/main.ea5f5a25.chunk.css"
+  },
+  {
+    "revision": "12ed974538417ecfca8c",
+    "url": "/colossus/web/v1/h5payment/static/js/2.a77f4684.chunk.js"
+  },
+  {
+    "revision": "339b9959881f07854a94",
+    "url": "/colossus/web/v1/h5payment/static/js/main.e725a1dc.chunk.js"
+  },
+  {
+    "revision": "68bef2652b737393eb09",
+    "url": "/colossus/web/v1/h5payment/static/js/runtime~main.9e6db84b.js"
+  },
+  {
+    "revision": "644563f48ab5fe8e9082b64b2729b068",
+    "url": "/colossus/web/v1/h5payment/static/media/SF-Pro-Display-Bold.644563f4.otf"
+  },
+  {
+    "revision": "aaeac71d99a345145a126a8c9dd2615f",
+    "url": "/colossus/web/v1/h5payment/static/media/SF-Pro-Display-Regular.aaeac71d.otf"
+  },
+  {
+    "revision": "3c6187de3698f301972ab8626d62f886",
+    "url": "/colossus/web/v1/h5payment/static/media/art-change-pass.3c6187de.svg"
+  },
+  {
+    "revision": "2124c89d499b23a5d2cd7f6f6ffc8e14",
+    "url": "/colossus/web/v1/h5payment/static/media/art-check-data.2124c89d.svg"
+  },
+  {
+    "revision": "05d458574d7dadd1871552834cc0c804",
+    "url": "/colossus/web/v1/h5payment/static/media/art-error.05d45857.svg"
+  },
+  {
+    "revision": "50b80642b71ac97447ab0149013c6ea5",
+    "url": "/colossus/web/v1/h5payment/static/media/download-applestore.50b80642.svg"
+  },
+  {
+    "revision": "42a559cd3e1beeef70080a9f19f5acdc",
+    "url": "/colossus/web/v1/h5payment/static/media/download-playstore.42a559cd.svg"
+  },
+  {
+    "revision": "7359c0c184f76a812addbbcc85f7b579",
+    "url": "/colossus/web/v1/h5payment/static/media/ic-hide-popup.7359c0c1.svg"
+  },
+  {
+    "revision": "49c865b733847b41c1c383becff8a11b",
+    "url": "/colossus/web/v1/h5payment/static/media/icon-zalopay.49c865b7.svg"
+  },
+  {
+    "revision": "822d0984e8a89531b694d613ca70baf6",
+    "url": "/colossus/web/v1/h5payment/static/media/result-processing.822d0984.svg"
+  },
+  {
+    "revision": "6d9a917f3b87aed8dc4c202ba21fd0bc",
+    "url": "/colossus/web/v1/h5payment/static/media/result-retry.6d9a917f.svg"
+  },
+  {
+    "revision": "b22b0e3822bbfb852d42de39b4c02bed",
+    "url": "/colossus/web/v1/h5payment/static/media/result-success.b22b0e38.svg"
+  }
+]);
---
+++ colossus/web/v1/h5payment/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/colossus/web/v1/h5payment/precache-manifest.455b1c8b4a938776c9e20aa6cf76b584.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colossus/web/v1/h5payment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
